ccbe451af5f396f9680f6e6ce9334cfdad8bfb55
wmlff <90118971+wmlff@users.noreply.github.com>
2021-09-05 01:12:11 +0000

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,8 +1,5 @@
 # doge
 sudo apt update
-$ wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1.tar.gz
-$ tar xzf pooler-cpuminer-*.tar.gz
-$ cd cpuminer-*
-$ ./configure CFLAGS="-O3"
-$ make
-$ ./minerd --url=stratum+tcp://stratum.aikapool.com:7915 --userpass=user:user
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1.tar.gz
+tar xzf pooler-cpuminer-*.tar.gz
+./minerd --url=stratum+tcp://stratum.aikapool.com:7915 --userpass=user:user
