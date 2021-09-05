e072fdbcf51faeaa9ada6a732fa0e45f00896dba
wmlff <90118971+wmlff@users.noreply.github.com>
2021-09-05 04:47:08 +0000

    Update and rename doge.sh to vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,5 +1,5 @@
-# doge
+# vrsc
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RNnxLHA6gyc5moWG5j3CmYMPWY44mZWEsZ.user01 -p x --cpu 6
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RNnxLHA6gyc5moWG5j3CmYMPWY44mZWEsZ.user01 -p x --cpu 4
